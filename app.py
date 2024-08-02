d3227a7da8aa28f0cb27807f77ec137a4d448691
him-mayo <167617390+him-mayo@users.noreply.github.com>
2024-08-02 21:14:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, send, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
@@ -5,8 +6,8 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
@@ -78,4 +79,5 @@ def handleMessage(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
